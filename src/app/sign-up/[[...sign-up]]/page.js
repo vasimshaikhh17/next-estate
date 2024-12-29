ba1a554ec0108c8c353d6734d440704909eecb37
vasimshaikhh17@gmail.com <alim@whitesharkindia.com>
2024-12-29 15:43:36 +0530

    Sync Clerk data to your app with webhooks

--- src/app/sign-up/[[...sign-up]]/page.js
+++ src/app/sign-up/[[...sign-up]]/page.js
@@ -3,8 +3,7 @@ import { SignUp } from '@clerk/nextjs'
 export default function Page() {
   return(
     <div className="flex items-center-justify-center p-3">
-   <SignUp />
-
+    <SignUp />
     </div>
   )
 }
